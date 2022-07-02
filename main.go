23079513be6b4917ed1da67d3219f98090f85ee2
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2022-07-03 06:30:04 +0900

    Blockの追加と表示

--- main.go
+++ main.go
@@ -3,26 +3,28 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 )
 
+//Block
 type Block struct {
 	nonce        int
 	previousHash string
 	timestamp    int64
-
 	//複数のトランザクション
 	transactions []string
 }
 
-//ブロックのプリント用メソッド
+//Blockのプリント用メソッド
 func (b *Block) Print() {
-	fmt.Printf("timestamp    %d\n", b.timestamp)
-	fmt.Printf("noce         %d\n", b.nonce)
-	fmt.Printf("previousHash %s\n", b.previousHash)
-	fmt.Printf("transactions %s\n", b.transactions)
+	fmt.Printf("timestamp    : %d\n", b.timestamp)
+	fmt.Printf("noce         : %d\n", b.nonce)
+	fmt.Printf("previousHash : %s\n", b.previousHash)
+	fmt.Printf("transactions : %s\n", b.transactions)
 }
 
+//新規Block作成
 func NewBlock(nonce int, previousHash string) *Block {
 	b := new(Block)
 	b.timestamp = time.Now().UnixNano()
@@ -31,11 +33,43 @@ func NewBlock(nonce int, previousHash string) *Block {
 	return b
 }
 
+//BlockChain
+type BlockChain struct {
+	transactionPool []string
+	//Blockの配列
+	chain []*Block
+}
+
+//BlockChainの作成（初期化）
+func NewBlockChain() *BlockChain {
+	bc := new(BlockChain)
+	bc.AddBlock(0, "initHash")
+	return bc
+}
+
+//BlockをChainに追加するメソッド
+func (bc *BlockChain) AddBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+//BlockChainのプリント用メソッド
+func (bc *BlockChain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
+}
+
 func init() {
 	log.SetPrefix("BlockChain: ")
 }
 
 func main() {
-	b := NewBlock(0, "testHash")
-	b.Print()
+	blockChain := NewBlockChain()
+	blockChain.AddBlock(1, "test 1")
+	blockChain.AddBlock(2, "test 2")
+	blockChain.Print()
 }

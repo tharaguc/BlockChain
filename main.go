e303f0a022d99a9988b578b95019185ecda5619f
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2022-07-03 07:09:25 +0900

    previousHashを適用したBlockの作成

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
@@ -10,7 +12,7 @@ import (
 //Block
 type Block struct {
 	nonce        int
-	previousHash string
+	previousHash [32]byte
 	timestamp    int64
 	//複数のトランザクション
 	transactions []string
@@ -20,12 +22,33 @@ type Block struct {
 func (b *Block) Print() {
 	fmt.Printf("timestamp    : %d\n", b.timestamp)
 	fmt.Printf("noce         : %d\n", b.nonce)
-	fmt.Printf("previousHash : %s\n", b.previousHash)
+	fmt.Printf("previousHash : %x\n", b.previousHash)
 	fmt.Printf("transactions : %s\n", b.transactions)
 }
 
+//BlockのHash化
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+	return sha256.Sum256(m)
+}
+
+//適切にJSONMarshalするメソッド（json.Marshalの上書き）
+func (b *Block) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Timestamp    int64    `json:"timestamp"`
+		Nonce        int      `json:"nonce"`
+		PreviousHash [32]byte `json:"previous_hash"`
+		Transactions []string `json:"transactions"`
+	}{
+		Timestamp:    b.timestamp,
+		Nonce:        b.nonce,
+		PreviousHash: b.previousHash,
+		Transactions: b.transactions,
+	})
+}
+
 //新規Block作成
-func NewBlock(nonce int, previousHash string) *Block {
+func NewBlock(nonce int, previousHash [32]byte) *Block {
 	b := new(Block)
 	b.timestamp = time.Now().UnixNano()
 	b.nonce = nonce
@@ -42,22 +65,33 @@ type BlockChain struct {
 
 //BlockChainの作成（初期化）
 func NewBlockChain() *BlockChain {
+	//Genesis Block
+	b := &Block{}
 	bc := new(BlockChain)
-	bc.AddBlock(0, "initHash")
+	bc.AddBlock(0, b.Hash())
 	return bc
 }
 
 //BlockをChainに追加するメソッド
-func (bc *BlockChain) AddBlock(nonce int, previousHash string) *Block {
+func (bc *BlockChain) AddBlock(nonce int, previousHash [32]byte) *Block {
 	b := NewBlock(nonce, previousHash)
 	bc.chain = append(bc.chain, b)
 	return b
 }
 
+//最後のBlockを返すメソッド
+func (bc *BlockChain) LastBlock() *Block {
+	return bc.chain[len(bc.chain)-1]
+}
+
 //BlockChainのプリント用メソッド
 func (bc *BlockChain) Print() {
 	for i, block := range bc.chain {
-		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		if i == 0 {
+			fmt.Printf("%s Genesis Block %s\n", strings.Repeat("=", 25), strings.Repeat("=", 25))
+		} else {
+			fmt.Printf("%s Block %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		}
 		block.Print()
 	}
 	fmt.Printf("%s\n", strings.Repeat("*", 25))
@@ -69,7 +103,10 @@ func init() {
 
 func main() {
 	blockChain := NewBlockChain()
-	blockChain.AddBlock(1, "test 1")
-	blockChain.AddBlock(2, "test 2")
+
+	preHash := blockChain.LastBlock().Hash()
+	blockChain.AddBlock(1, preHash)
+	preHash = blockChain.LastBlock().Hash()
+	blockChain.AddBlock(2, preHash)
 	blockChain.Print()
 }
